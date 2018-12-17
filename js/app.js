3f3afc83243697a4998525b35fb8d8b3fea0ee78
Cornelis Seevinck <corky@seevinck.com>
2018-12-16 18:52:59 -0700

    Feat: Constants & background & canvas spacing
    - put game constants in the front as const xxxx
    - insert bacjground image
    - adjust canvas spacing at top

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 // Declare enemies and Player
 let allEnemies = [];
 let player = {};
+
+// Constants for the game
+const canvasLeft = 0; // left side of canvas
+const canvasRight = 400; // right side of canvas
+const playerStartx = 200; // player start x coordinate
+const playerStarty = 366; // player start y coordinate
+const playerVertMov = 83; // player vertical movement adjustment (same as enemyRowDepth)
+const enemyRowDepth = 83; // distance between enemy rows
+const playerHorzMov = 100; // player horizontal movement adjustment
+const enemyStartx = -100; // enemy start x coordinate
+const enemyOverEndx = 100; // enemy end x coordinate beyond canvas before wrap
+const enemyStarty = 34; // enemy start y coordinate
 const speedInc = 25; // increase factor for speed (levels)
 
 /**
- * Enemies player must avoid
+ * Enemies Class - player must avoid these
  *
  * @class Enemy
  */
@@ -17,7 +29,7 @@ class Enemy {
      * @memberof Enemy
      */
     constructor(x, y, speed) {
-        // Variables applied to each of our instances go here,
+        // Variables for each of instance of enemy
         this.x = x;
         this.y = y;
         this.speed = speed;
@@ -34,15 +46,15 @@ class Enemy {
      */
     update(dt) {
 
-        // Multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for all computers
-        // level * speedInc increases the speed after each win
+        // Multiply any movement by the dt parameter which ensures
+        // the game runs at the same speed for all computers
+        // level * speedInc increases the speed after each level win
         this.x += (50 + (player.level * speedInc) + this.speed) * dt;
 
 
         // If this enemy is at the end of the row -> back to start
-        if (this.x > 550) {
-            this.resetRow(-100);
+        if (this.x > canvasRight + enemyOverEndx) {
+            this.resetRow(enemyStartx);
         };
     };
 
@@ -56,7 +68,7 @@ class Enemy {
     };
 
     /**
-     * Reset game row for one ememy & get speed for its next run
+     * Reset game row for one ememy & calculate speed for its next run
      *
      * @param {*} x - the row to reset
      * @memberof Enemy
@@ -81,7 +93,7 @@ class Player {
      */
     constructor(x, y) {
 
-        // Variables applied to player go here
+        // Player variables
         this.x = x;
         this.y = y;
         this.sprite = 'images/char-boy.png'; // The image/sprite for player
@@ -97,7 +109,9 @@ class Player {
      * @memberof Player
      */
     update() {
-        // Don't do update if we have reached the water
+        // Don't do update if we have reached the water. Want the player to
+        // render for a short time - looks cool
+
         if (this.turnEnded) {
             return;
         };
@@ -105,14 +119,12 @@ class Player {
         // Check for collision
         for (let enemy of allEnemies) {
             if (this.y === enemy.y && (enemy.x + 40 >= this.x && enemy.x - 40 <= this.x)) {
-                if (!this.turnEnded) {
-                    this.losses++;
-                    this.turnEnded = true;
-                };
+                this.losses++;
+                this.turnEnded = true;
             };
         };
 
-        // If we just found a collition - reset all ememy and player positions
+        // If we just found a collision - reset all ememy and player positions
         if (this.turnEnded) {
             this.restartTurn();
             this.level = 0; // back to level 0 and normal speed
@@ -120,12 +132,12 @@ class Player {
     };
 
     /**
-     * Check if player reached the water (game win)
+     * Check if player reached the water (level win)
      *
      * @memberof Player
      */
-    reachedWater() {
-        if (this.y < 68) {
+    checkForAndDoWin() {
+        if (this.y < enemyStarty) {
             this.wins++;
             this.turnEnded = true;
             this.level++; // increase level & enemy speed
@@ -145,24 +157,23 @@ class Player {
      * @memberof Player
      */
     restartGame() {
-        this.restartTurn();
         this.wins = 0;
         this.losses = 0;
         this.level = 0; // back to level 0 and normal speed
+        this.restartTurn();
     };
 
     /**
-     * restartTurn - called when the player has reached the water
-     * or when a collision has accurred. 
+     * restartTurn - called when the player reached the water or on collision
      * Reset enemy and player positions. 
      *
      * @memberof Player
      */
     restartTurn() {
-        this.x = 200; // reset player x
-        this.y = 400; // reset player y
+        this.x = playerStartx; // reset player x
+        this.y = playerStarty; // reset player y
         allEnemies.forEach(function(enemy) {
-            enemy.resetRow(-100);
+            enemy.resetRow(enemyStartx);
         });
         this.turnEnded = false; // allow keystrokes again
     };
@@ -174,7 +185,7 @@ class Player {
      */
     render() {
 
-        // Store win and loss counts into dom elements
+        // Store win and loss counts & level into dom elements
         document.getElementById("win-cnt").innerHTML = this.wins;
         document.getElementById("loss-cnt").innerHTML = this.losses;
         document.getElementById("level-num").innerHTML = this.level;
@@ -195,26 +206,26 @@ class Player {
         }; // ignore keystrokes while previous turn finishes
         switch (key) {
             case 'left':
-                if (this.x > 50) {
-                    this.x -= 100;
+                if (this.x > canvasLeft) { // left edge of canvas
+                    this.x -= playerHorzMov;
                 };
                 break;
             case 'right':
-                if (this.x < 350) {
-                    this.x += 100;
+                if (this.x < canvasRight) { // right edge of canvas
+                    this.x += playerHorzMov;
                 };
                 break;
             case 'up':
-                if (this.y > 67) {
-                    this.y -= 83;
+                if (this.y >= enemyStarty) {
+                    this.y -= playerVertMov;
                 };
 
                 // Process "win" if we reached water
-                this.reachedWater();
+                this.checkForAndDoWin();
                 break;
             case 'down':
-                if (this.y < 350) {
-                    this.y += 83;
+                if (this.y < playerStarty) {
+                    this.y += playerVertMov;
                 };
                 break;
             default:
@@ -227,11 +238,11 @@ class Player {
  *  Place all enemy objects in an array called allEnemies
  *  Place the player object in a variable called player
  */
-player = new Player(200, 400);
+player = new Player(playerStartx, playerStarty);
 allEnemies = [
-    new Enemy(-100, 68, 7000),
-    new Enemy(-100, 151, 9000),
-    new Enemy(-100, 234, 8000)
+    new Enemy(enemyStartx, enemyStarty, 7000),
+    new Enemy(enemyStartx, enemyStarty + enemyRowDepth, 9000),
+    new Enemy(enemyStartx, enemyStarty + (enemyRowDepth * 2), 8000)
 ];
 
 /*
